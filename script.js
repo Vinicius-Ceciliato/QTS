4c4b6235d7e54470a82c56e2bc51eb2f75b0edc9
Vinicius-Ceciliato <viniceciliato2008@gmail.com>
2026-05-25 10:53:16 -0300

    fazendo exercicios

--- script.js
+++ script.js
@@ -6,4 +6,50 @@ function mensagemBoasVindas(nome) {
     return `Olá, ${nome}!`;
 }
 
-module.exports = { somar, mensagemBoasVindas };
+function maiorValor(a, b) {
+    if (a>b){return alert(a)}
+    else {return alert(b)}
+}
+
+function parImpar(a) {
+    if(a%2==0) {
+        return 'par'
+    } else {return 'impar'};
+};
+
+function idade(i) {
+    if(i>=18) {return 'pode votar'};
+    if(i<18 && i>=16) {return 'voto opcional'}
+    else {'não pode voltar'};
+}
+
+function positivoNegativo(a) {
+    if (a>0) {return 'positivo'};
+    if (a<0) {return 'negativo'};
+    if (a==0) {return 'zero'};
+}
+
+function salario(dinheiro) {
+    if(dinheiro<2000){
+        bonus = dinheiro * 0.2;
+        return bonus;
+    };
+    if(dinheiro>=2000 && dinheiro<=5000) {
+        bonus = dinheiro * 0.1;
+        return bonus;
+    };
+    if(dinheiro>5000) {
+        bonus = dinheiro * 0.05;
+        return bonus;
+    };
+    
+}
+
+function habilitacao(idade) {
+        if(idade >= 18) {return 'pode dirigir'}
+        else {return 'não pode dirigir'};
+}
+
+
+
+module.exports = { somar, mensagemBoasVindas, maiorValor, parImpar, positivoNegativo, idade, salario, habilitacao};
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const { somar, mensagemBoasVindas } = require('./script');
+const { somar, mensagemBoasVindas, maiorValor, parImpar, positivoNegativo, salario} = require('./script');
 
 test('soma dois números', () => {
     expect(somar(2, 3)).toBe(5);
@@ -7,3 +7,27 @@ test('soma dois números', () => {
 test('mensagem de boas vindas', () => {
     expect(mensagemBoasVindas('Diogo')).toBe('Olá, Diogo!');
 });
+
+test('maior numero', () => {
+    expect(maiorValor(5, 3)).toBe(5);
+});
+
+test('par ou ímpar', () => {
+    expect(parImpar(4)).toBe('par');
+});
+
+test('voto', () => {
+    expéct(idade(18).toBe('pode votar'))
+});
+
+test('positivo ou negativo', () => {
+    expect(positivoNegativo(2).toBe('positivo'));
+});
+
+test('salario e bonus', () => {
+    expect(salario(6000).toBe(dinheiro*0.1));
+});
+
+test('habilitacao', () => {
+    expect(idade(18).toBe('pode dirigir'))
+})
